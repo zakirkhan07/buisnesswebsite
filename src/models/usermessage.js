4d68b6b5447a3c2075462c77209acd0e05a9ea34
zakirkhan786 <53606059+zakirkhan786@users.noreply.github.com>
2021-10-01 01:07:39 +0530

    project

---
+++ src/models/usermessage.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const validator = require("validator");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required:true,
+        minLength:3
+    },
+    email:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email Id")
+            }
+        }
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    message:{
+        type: String,
+        required:true,
+    }
+})
+
+// we need to create collection
+
+const User = mongoose.model( "User",userSchema)
+
+module.exports =  User;
